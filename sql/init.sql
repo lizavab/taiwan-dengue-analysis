060080cc04d08afb80ad06ad62b7fe5c573bc71e
Liza <97094617+lizavab@users.noreply.github.com>
2025-03-04 11:35:03 +0100

    Update shp-file insert

--- sql/init.sql
+++ sql/init.sql
@@ -6,80 +6,87 @@ DROP TABLE IF EXISTS
     townships
 CASCADE;
 
-CREATE TABLE "dengue_cases"(
-	"id" INTEGER NOT NULL,
-	"date_onset" DATE,
-	"date_confirmation" DATE,
-	"date_notification" DATE,
-	"sex" TEXT,
-	"age_group" TEXT,
-	"imported" TEXT,
-	"country_infected" TEXT,
-	"confirmed_cases" INTEGER,
-	"serotype" TEXT,
-	"township_living" INTEGER,
-	"township_infected" INTEGER
+CREATE TABLE dengue_cases(
+	id INTEGER NOT NULL,
+	date_onset DATE,
+	date_confirmation DATE,
+	date_notification DATE,
+	sex TEXT,
+	age_group TEXT,
+	imported TEXT,
+	country_infected TEXT,
+	confirmed_cases INTEGER,
+	serotype TEXT,
+	township_living INTEGER,
+	township_infected INTEGER
 );
-ALTER TABLE
-	"dengue_cases" ADD PRIMARY KEY("id");
-CREATE TABLE "weather_reports"(
-	"id" INTEGER NOT NULL,
-	"station" TEXT NOT NULL,
-	"obs_date" DATE NOT NULL,
-	"temp" FLOAT,
-	"temp_max" FLOAT,
-	"temp_min" FLOAT,
-	"temp_dp" FLOAT,
-	"rel_humidity" INTEGER,
-	"wind_speed" FLOAT,
-	"wind_dir" FLOAT,
-	"precp" FLOAT
-);
-ALTER TABLE
-	"weather_reports" ADD PRIMARY KEY("id");
-/*CREATE TABLE "counties"(
-	"code" INTEGER NOT NULL,
-	"name" TEXT,
-	"orig_name" TEXT,
-	"total_pop" INTEGER,
-	"geom" GEOMETRY NOT NULL
-);
-ALTER TABLE
-	"counties" ADD PRIMARY KEY("code");*/
-CREATE TABLE "townships"(
-	"code" INTEGER NOT NULL,
-	"county" INTEGER NOT NULL,
-	"id" TEXT NOT NULL,
-	"name" TEXT,
-	"orig_name" TEXT,
-	"total_pop" INTEGER,
-	"geom" GEOMETRY NOT NULL
-);
-ALTER TABLE
-	"townships" ADD PRIMARY KEY("code");
-CREATE TABLE "stations"(
-	"code" TEXT NOT NULL,
-	"township" INTEGER,
-	"name" TEXT,
-	orig_name TEXT,
-	"type" TEXT,
-	"longitude" FLOAT,
-	"latitude" FLOAT,
-	"altitude" FLOAT,
-	"data_start_date" DATE,
-	"data_end_date" DATE,
-	"geom" GEOMETRY
-);
-ALTER TABLE
-	"stations" ADD PRIMARY KEY("code");
 
-/*ALTER TABLE
-	"townships" ADD CONSTRAINT "townships_county_foreign" FOREIGN KEY("county") REFERENCES "counties"("code");*/
 ALTER TABLE
-	"dengue_cases" ADD CONSTRAINT "dengue_cases_township_infected_foreign" FOREIGN KEY("township_infected") REFERENCES "townships"("code");
+	dengue_cases ADD PRIMARY KEY(id);
+	
+CREATE TABLE weather_reports(
+	id SERIAL PRIMARY KEY,
+	station TEXT NOT NULL,
+	obs_date DATE NOT NULL,
+	temp FLOAT,
+	temp_max FLOAT,
+	temp_min FLOAT,
+	temp_dp FLOAT,
+	rel_humidity FLOAT,
+	wind_speed FLOAT,
+	wind_dir FLOAT,
+	precp FLOAT
+);
+
+CREATE TABLE counties(
+	code INTEGER NOT NULL,
+	id TEXT NOT NULL,
+	name TEXT,
+	orig_name TEXT,
+	total_pop INTEGER,
+	geom GEOMETRY NOT NULL
+);
+
 ALTER TABLE
-	"weather_reports" ADD CONSTRAINT "weather_reports_station_foreign" FOREIGN KEY("station") REFERENCES "stations"("code");
+	counties ADD PRIMARY KEY(code);
+	
+CREATE TABLE townships(
+	code INTEGER NOT NULL,
+	county INTEGER NOT NULL,
+	id TEXT NOT NULL,
+	name TEXT,
+	orig_name TEXT,
+	total_pop INTEGER,
+	geom GEOMETRY NOT NULL
+);
+
 ALTER TABLE
-	"dengue_cases" ADD CONSTRAINT "dengue_cases_township_living_foreign" FOREIGN KEY("township_living") REFERENCES "townships"("code");
+	townships ADD PRIMARY KEY(code);
+	
+CREATE TABLE stations(
+	code TEXT NOT NULL,
+	township INTEGER,
+	name TEXT,
+	orig_name TEXT,
+	type TEXT,
+	longitude FLOAT,
+	latitude FLOAT,
+	altitude FLOAT,
+	data_start_date DATE,
+	data_end_date DATE,
+	geom GEOMETRY
+);
 ALTER TABLE
-	"stations" ADD CONSTRAINT "stations_township_foreign" FOREIGN KEY("township") REFERENCES "townships"("code");
+	stations ADD PRIMARY KEY(code);
+
+ALTER TABLE townships ADD CONSTRAINT townships_county_foreign FOREIGN KEY(county) REFERENCES counties(code);
+
+ALTER TABLE weather_reports ADD CONSTRAINT weather_reports_station_foreign FOREIGN KEY(station) REFERENCES stations(code);
+
+ALTER TABLE dengue_cases ADD CONSTRAINT dengue_cases_township_infected_foreign FOREIGN KEY(township_infected) REFERENCES townships(code);
+
+ALTER TABLE dengue_cases ADD CONSTRAINT dengue_cases_township_living_foreign FOREIGN KEY(township_living) REFERENCES townships(code);
+
+ALTER TABLE stations ADD CONSTRAINT stations_township_foreign FOREIGN KEY(township) REFERENCES townships(code);
+
+
--- sql/insert_counties.sql
+++ sql/insert_counties.sql
@@ -1,22 +1,18 @@
 /* Run in command line:
-/path/to/shp2pgsql -s 4326 -W "UTF-8" TOWN_MOI_1120317.shp temporary_table | psql -d database_name -U username
+/path/to/shp2pgsql -s 4326 -W "UTF-8" COUNTY_MOI_1090820.shp temporary_table | psql -d database_name -U username
 
 Parameters:
 -s 4326: Specifies the coordinate system. 4326 is for WGS84.
 -W "UTF-8": Sets the encoding for text attributes. */
 
 -- Upload township geometries to a temporary table.
-
 ALTER TABLE temporary_table
-  ALTER COLUMN towncode TYPE INTEGER USING (towncode::integer),
   ALTER COLUMN countycode TYPE INTEGER USING (countycode::integer);
   
 -- Transfer to the predefined table.
-
-INSERT INTO townships (code, county, id, name, orig_name, total_pop, geom)
-SELECT towncode, countycode, townid, towneng, townname, NULL, geom
+INSERT INTO counties (code, id, name, orig_name, total_pop, geom)
+SELECT countycode, countyid, countyeng, countyname, NULL, geom
 FROM temporary_table;
 
 -- Delete the temporary table.
-
 DROP TABLE IF EXISTS temporary_table;
