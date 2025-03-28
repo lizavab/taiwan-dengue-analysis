7ddb4c05b83403f60bf93f968a4477e4ea9a16f4
Liza <97094617+lizavab@users.noreply.github.com>
2025-03-28 14:09:39 +0100

    Update interpolation

--- scripts/interpolate_historical_weather.py
+++ scripts/interpolate_historical_weather.py
@@ -17,83 +17,102 @@ start_time = time.time()
 arcpy.env.overwriteOutput = True
 
 # Set environment settings
-env.workspace = "C:/Users/kgw721/Documents"
+env.workspace = "C:/Users/..."
 
 # Create a SQLAlchemy engine
 conn_string = "postgresql://postgres:password@localhost:5433/tw" # database connection string
 engine = create_engine(conn_string)
 
-# Insert township geom into geodataframe
-townships = gpd.read_postgis(
-    "SELECT county, code, name, pop_density, geom FROM main_island_townships",
-    engine,
-    geom_col="geom"
-)
+# Specify path for observations subset and raster
+shp_path = "obs.shp"
+raster_path = "nn_raster.tif"
 
-query = "SELECT w.*, s.geom FROM weather_2011_2024 w JOIN stations s ON s.code = w.station WHERE year = 2012 AND month = 8"
-subset = gpd.read_postgis(
-    query,
-    engine,
-    geom_col="geom"
-)
+# Create an empty dataframe to store all interpolated data for 2011-2024
+interpolated_data = pd.DataFrame(columns=['county', 'code', 'name', 'year', 'month', 'tavg', 'tmax', 'tmin', 'wind_speed', 'precip'])
 
-print(subset.head(5))
+#years = list(range(2011, 2025))
+#months = list(range(1, 13))
 
-shp_path = "C:/Users/kgw721/Documents/subset.shp"
-subset.to_file(shp_path, driver='ESRI Shapefile')
-print(shp_path)
+years = list(range(2012, 2013)) # test with one year (2012)
+months = list(range(1, 13))
 
+for year in years:
+    for month in months:
+        print(f"Processing {year}/{month}.")
 
-variables = ['tavg', 'tmax', 'tmin', 'wind_speed', 'precip']
+        # Use placeholders for year and month
+        query = "SELECT w.*, s.geom FROM weather_2011_2024 w JOIN stations s ON s.code = w.station WHERE year = %s AND month = %s"
 
-for variable in variables:
-    print(f"Interpolating {variable}.")
-    raster = NaturalNeighbor(shp_path, variable, 2500)
-    raster_path = "C:/Users/kgw721/Documents/nn_raster.tif"
-    raster.save(raster_path)
+        # Insert weather observations and station geometries into geodataframe
+        obs = gpd.read_postgis(
+            query,
+            engine,
+            params=(year, month),
+            geom_col="geom"
+        )
+        print(obs.head(5))
 
-    # Calculate zonal statistics
-    print(f"Calculating zonal statistics for {variable}.")
-    if variable == 'tmax':
-        zs = zonal_stats(townships, raster_path, stats=['max'])
-        print(zs)
+        obs.to_file(shp_path, driver='ESRI Shapefile')
+        print(shp_path)
 
-        stats = pd.DataFrame(zs)
-        stats.rename(columns={'max':'tmax'}, inplace=True)
+        # Insert township geom into geodataframe
+        townships = gpd.read_postgis(
+            "SELECT county, code, name, geom FROM main_island_townships",
+            engine,
+            geom_col="geom"
+        )
 
-    elif variable == 'tmin':
-        zs = zonal_stats(townships, raster_path, stats=['min'])
-        print(zs)
+        variables = ['tavg', 'tmax', 'tmin', 'wind_speed', 'precip']
 
-        stats = pd.DataFrame(zs)
-        stats.rename(columns={'min':'tmin'}, inplace=True)
-    else:
-        zs = zonal_stats(townships, raster_path, stats=['mean'])
-        print(zs)
-        if variable == 'tavg':
-            stats = pd.DataFrame(zs)
-            stats.rename(columns={'mean':'tavg'}, inplace=True)
+        for variable in variables:
+            print(f"Interpolating {variable}.")
+            raster = NaturalNeighbor(shp_path, variable, 2500)
+            raster.save(raster_path)
 
-        elif variable == 'wind_speed':
-            stats = pd.DataFrame(zs)
-            stats.rename(columns={'mean':'wind_speed'}, inplace=True)
+            # Calculate zonal statistics
+            print(f"Calculating zonal statistics for {variable}.")
+            if variable == 'tmax':
+                zs = zonal_stats(townships, raster_path, stats=['max'], all_touched=True)
 
-        else:
-            stats = pd.DataFrame(zs)
-            stats.rename(columns={'mean':'precip'}, inplace=True)
+                stats = pd.DataFrame(zs)
+                stats.rename(columns={'max':'tmax'}, inplace=True)
 
-    # Merge with townships
-    townships = pd.concat([townships, stats], axis=1)
-    print(townships.head(5))
+            elif variable == 'tmin':
+                zs = zonal_stats(townships, raster_path, stats=['min'], all_touched=True)
 
-    subset_2012_8 = townships.drop(columns=['geom'])
+                stats = pd.DataFrame(zs)
+                stats.rename(columns={'min':'tmin'}, inplace=True)
+            else:
+                zs = zonal_stats(townships, raster_path, stats=['mean'], all_touched=True)
 
-    # Add year and month
-    subset_2012_8['year'] = 2012
-    subset_2012_8['month'] = 8
+                if variable == 'tavg':
+                    stats = pd.DataFrame(zs)
+                    stats.rename(columns={'mean':'tavg'}, inplace=True)
 
-    subset_2012_8.to_csv("C:/Users/kgw721/Documents/subset_2012_8.csv", index=False)
+                elif variable == 'wind_speed':
+                    stats = pd.DataFrame(zs)
+                    stats.rename(columns={'mean':'wind_speed'}, inplace=True)
 
+                else:
+                    stats = pd.DataFrame(zs)
+                    stats.rename(columns={'mean':'precip'}, inplace=True)
+
+            # Merge with townships
+            townships = pd.concat([townships, stats], axis=1)
+            print(townships.head(5))
+
+        # Remove geometry
+        subset = townships.drop(columns=['geom'])
+
+        # Add year and month
+        subset['year'] = year
+        subset['month'] = month
+
+        # Insert subset into main dataframe
+        interpolated_data = pd.concat([interpolated_data, subset[interpolated_data.columns]], ignore_index=True)
+        print(interpolated_data.tail(5))
+
+interpolated_data.to_csv("interpolated_data.csv", index=False)
 
 total_time = time.time() - start_time
 print(f"Processing time: {total_time:.2f} seconds")
