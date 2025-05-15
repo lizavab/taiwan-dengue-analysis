1d06eda768d5d689e8bba6b8c1cea36f96d66d20
Liza <97094617+lizavab@users.noreply.github.com>
2025-05-15 23:51:53 +0200

    Improved script

--- scripts/visualise_data.R
+++ scripts/visualise_data.R
@@ -19,7 +19,7 @@ cases_heatmap <-
   geom_raster() +
   ylab("") + 
   xlab("") +
-  scale_fill_gradientn(name = "log(DCC)", colours = brewer.pal(9, "Reds"), 
+  scale_fill_gradientn(name = "DCC (log)", colours = brewer.pal(9, "Reds"), 
                        trans = "log1p", 
                        breaks = c(0, 10, 100, 1000), 
                        labels = c(0, 10, 100, 1000)) + 
@@ -38,21 +38,21 @@ cases_heatmap <-
     legend.key.size = unit(1, "cm")
   )
 
-ggsave("cases_heatmap.png", height = 20, width = 23, units = "cm")
+ggsave("cases_heatmap.png", height = 15, width = 18, units = "cm")
 
-# Dengue incidence rate (DIR) heatmap
-dir_heatmap <- 
+# Dengue incidence (DI) heatmap
+incidence_heatmap <- 
   data %>% 
   group_by(year, month) %>%
   summarise(cases = sum(case_count),
             pop = sum(total_pop)) %>% 
-  # Calculate DIR (per 100,000 population)
+  # Calculate DI (per 100,000 population)
   mutate(var = cases / pop * 10^5) %>% 
   ggplot(aes(x = month, y = year, fill = var)) + 
   geom_raster() +
   ylab("") + 
   xlab("") +
-  scale_fill_gradientn(name = "log(DIR)", colours = brewer.pal(9, "Reds"), 
+  scale_fill_gradientn(name = "DI (log)", colours = brewer.pal(9, "Reds"), 
                        trans = "log1p", 
                        breaks = c(0, 1, 5, 20), 
                        labels = c(0, 1, 5, 20)) + 
@@ -70,24 +70,24 @@ dir_heatmap <-
     legend.key.size = unit(1, "cm")
   )
 
-ggsave("dir_heatmap.png", height = 20, width = 23, units = "cm")
+ggsave("incidence_heatmap.png", height = 15, width = 18, units = "cm")
 
-# Annual dengue incidence rate (DIR) by township
-dir_annual <- 
+# Annual dengue incidence (DI) by township
+incidence_annual <- 
   data %>% 
   group_by(year, code) %>%
-  # Calculate annual DIR (per 100,000 population)
+  # Calculate annual DI (per 100,000 population)
   summarise(cases = sum(case_count),
-            pop = sum(total_pop)) %>% 
+            pop = first(total_pop)) %>% 
   mutate(var = cases / pop * 10^5)  %>%
   # Add townships
   left_join(map, ., by = c("TOWNCODE" = "code")) %>% 
   ggplot() + 
   geom_sf(aes(fill = var), lwd = 0, color = NA) +
-  scale_fill_gradientn(name = "log(DIR)", colours = brewer.pal(9, "Reds"), 
-                       trans = "log1p", breaks = c(0, 10, 100), 
-                       labels = c(0, 10, 100) ) + 
+  scale_fill_gradientn(name = "DI (log)", colours = brewer.pal(9, "Reds"), 
+                       trans = "log1p", breaks = c(0, 10, 100, 1000), 
+                       labels = c(0, 10, 100, 1000) ) + 
   theme_void() +
   facet_wrap(~year, ncol = 5)
 
-ggsave("dir_annual.pdf", height = 20, width = 30, units = "cm")
+ggsave("incidence_annual.pdf", height = 20, width = 30, units = "cm")
